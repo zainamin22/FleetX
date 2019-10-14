a79a1979ab59ea6dd35fd6ff7d11fb3760de45bc
Zain Amin <zainamin22@live.com>
2019-10-14 23:45:47 +0500

    changed root password

--- src/main/java/com/zzeebee/config/DatabaseConfig.java
+++ src/main/java/com/zzeebee/config/DatabaseConfig.java
@@ -29,7 +29,7 @@ public class DatabaseConfig {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/iefleetx");
         dataSource.setUsername("root");
-        dataSource.setPassword("");
+        dataSource.setPassword("root");
         return dataSource;
     }
 
